4aae2c9accf40bac1455767d3f80bb703aea7026
Jurayevkh <jurayevkh@gmail.com>
2023-11-21 04:53:31 +0500

    Created BackgroundServiceDemo class

---
+++ BackgroundServices/BackgroundServiceDemo.cs
@@ -0,0 +1,24 @@
+﻿namespace BackgroundServices
+{
+    public class BackgroundServiceDemo : BackgroundService
+    {
+        private readonly ILogger<BackgroundServiceDemo> _logger;
+
+        public BackgroundServiceDemo(ILogger<BackgroundServiceDemo> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var period = new PeriodicTimer(TimeSpan.FromSeconds(15));
+
+            while (await period.WaitForNextTickAsync(stoppingToken))
+            {
+                _logger.LogInformation("Bu Background Service har 15 sekunda shu xabarni qayta qayta yozib turadi");
+
+            } ;
+        }
+    }
+}
+
