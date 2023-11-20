39dbfe2b838787403d41a80ac5a07fc1268e6dcb
Jurayevkh <jurayevkh@gmail.com>
2023-11-21 04:54:16 +0500

    Created HostedService class

---
+++ BackgroundServices/HostedService.cs
@@ -0,0 +1,25 @@
+﻿namespace BackgroundServices
+{
+    public class HostedServiceDemo : IHostedService
+    {
+        private readonly ILogger<HostedServiceDemo> _logger;
+
+        public HostedServiceDemo(ILogger<HostedServiceDemo> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("IHostedService interfacening StartAsync methodi ishga tushdi");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("IHostedService interfacening StopAsync methodi ishga tushdi");
+            return Task.CompletedTask;
+        }
+    }
+}
+
