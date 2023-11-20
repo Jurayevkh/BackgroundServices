25fd243225c3f83f0f919fd77fe3bbbca3f25d2e
Jurayevkh <jurayevkh@gmail.com>
2023-11-21 04:54:34 +0500

    Set up Program.cs

--- BackgroundServices/Program.cs
+++ BackgroundServices/Program.cs
@@ -1,4 +1,20 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BackgroundServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHostedService<HostedLIfeCycleServiceDemo>();
+builder.Services.AddHostedService<HostedServiceDemo>();
+//builder.Services.AddHostedService<BackgroundServiceDemo>();
+
+
+builder.Services.Configure<HostOptions>(options =>
+{
+    options.StartupTimeout = TimeSpan.FromMinutes(1);
+    options.ShutdownTimeout = TimeSpan.FromMinutes(1);
+    options.ServicesStartConcurrently = true;
+    options.ServicesStopConcurrently = false;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
